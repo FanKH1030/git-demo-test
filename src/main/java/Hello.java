b5952d9946f3a284852116504f4976ed4eb2be1c
admin <13557423233@163.com>
2019-11-17 01:41:53 +0800

    git test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by fandi on 2019/11/16 0016.
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello demo");
+        int n = 20;
+        int m = 30;
+        int x = m + n;
+        System.out.println("x" + x);
+        int y = add(x, m);
+        System.out.println("y=" + y);
+
+        System.out.println("你好 demo");
+//        Map<String,Integer> map = new HashMap<>();
+        Map map = new HashMap();
+    }
+
+    public static int add(int m, int n) {
+        return m + n;
     }
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,8 @@
+/**
+ * Created by fandi on 2019/11/16 0016.
+ */
+public class User {
+    private String username;
+    private int age;
+
+}
