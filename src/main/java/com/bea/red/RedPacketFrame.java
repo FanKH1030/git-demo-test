3c7a8dde50165ee75dab63a5ddb236af18b6605d
admin <13557423233@163.com>
2019-11-24 00:08:45 +0800

    red pic2

--- src/main/java/com/bea/red/RedPacketFrame.java
+++ src/main/java/com/bea/red/RedPacketFrame.java
@@ -22,7 +22,7 @@ public abstract class RedPacketFrame extends JFrame {
     private static final long serialVersionUID = 1L;
     
 //    private static final String DIR = "day11-code\\pic";
-    private static final String DIR = "src";
+    private static final String DIR = "./";
 
     private ArrayList<Integer> moneyList = null;
 
